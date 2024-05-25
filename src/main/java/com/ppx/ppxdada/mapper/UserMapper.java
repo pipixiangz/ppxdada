a83b717b2d6cb0801853a059bc9e17348aba9f47
ppx <763038134@qq.com>
2024-05-25 18:55:49 +0800

    Initial code

---
+++ src/main/java/com/ppx/ppxdada/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.ppx.ppxdada.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ppx.ppxdada.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * 
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
